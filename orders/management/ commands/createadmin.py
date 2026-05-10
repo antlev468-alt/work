bf2d9983c32f524cb914e81f2dd9ae4e87af47a9
antlev468-alt <antlev468@gmail.com>
2026-05-10 19:38:13 +0300

    add create admin

--- orders/management/ commands/createadmin.py
+++ orders/management/ commands/createadmin.py
@@ -1,13 +1,12 @@
-
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-    help = 'Creates superuser if not exists'
+    help = 'Creates superuser'
 
     def handle(self, *args, **options):
         if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'antlev468@gmail.com', 'porol6767')
+            User.objects.create_superuser('admin', 'antlev468@gmail.com', 'admin123456')
             self.stdout.write('Superuser created!')
         else:
             self.stdout.write('Superuser already exists.')
